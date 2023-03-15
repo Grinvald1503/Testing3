f04365c74e139c4b6913148a315d2e636efd4edd
Aleksandr <aleksanches123@mail.ru>
2023-03-15 07:16:47 +0300

    Начало

---
+++ src/main/java/com/example/testing3/Testing3Application.java
@@ -0,0 +1,13 @@
+package com.example.testing3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testing3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Testing3Application.class, args);
+    }
+
+}
